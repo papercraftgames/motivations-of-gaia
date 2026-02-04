549a4f71d4bb8f9e6211bc1a762b981d42d763fd
RyAnne Graff <ryanne@resist.design>
2026-02-04 12:56:58 -0500

    chore: (repo) Automatic commit.

---
+++ src/main/java/games/papercraft/MotivationsOfGaia.java
@@ -0,0 +1,18 @@
+package games.papercraft;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class MotivationsOfGaia extends JavaPlugin {
+
+    public MotivationsOfGaia(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        // Prefab stuff...
+    }
+}
